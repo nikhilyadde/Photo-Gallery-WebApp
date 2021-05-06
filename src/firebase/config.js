8c5eaee42224b0423e13d6584e3d12d3e5880a6f
nikhilyadde <64202354+nikhilyadde@users.noreply.github.com>
2021-05-06 21:07:42 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,13 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAOf8MmXKXvSrRhvpwXx67G_iglfmy5sYo",
-  authDomain: "photo-gallery-eab51.firebaseapp.com",
-  databaseURL: "https://photo-gallery-eab51-default-rtdb.firebaseio.com",
-  projectId: "photo-gallery-eab51",
-  storageBucket: "photo-gallery-eab51.appspot.com",
-  messagingSenderId: "784868220556",
-  appId: "1:784868220556:web:54edae930577baac3f65a9",
-  measurementId: "G-KVNDCVXTWG"
+    apiKey: "AIzaSyB5SaVofy-c6eoPK4spqQsNblY38jzvqE4",
+    authDomain: "chat-app-f1039.firebaseapp.com",
+    projectId: "chat-app-f1039",
+    storageBucket: "chat-app-f1039.appspot.com",
+    messagingSenderId: "279951292127",
+    appId: "1:279951292127:web:3ec1f7fabc98ff99cd1b6a",
+    measurementId: "G-CHVD3CNJ72"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
